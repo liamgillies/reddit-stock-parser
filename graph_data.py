5847b66fe56024e7fd7837b5acc92ee4b2a71be2
liamgillies <51301501+liamgillies@users.noreply.github.com>
2021-01-06 23:56:01 -0800

    finished graphing

--- graph_data.py
+++ graph_data.py
@@ -1,34 +1,39 @@
 import matplotlib.pyplot as plt
 
-def plotTicker(name):
-    f = open("reddit_data.txt", "r")
-    line = f.readline()
-    closing_prices = []
-    positivity_ratings = []
-    negativity_ratings = []
-    counter_arr = []
-    counter = 0
-    while(line):
-        if(len(line)<20):
-            line = f.readline()
-            continue
-        line = line[1:-2].split(", ")
-        line[0] = line[0][1:-1]
-        if(line[0] != name):
-            line = f.readline()
-            continue
-        line[1:4] = [float(line[i]) for i in range(1,4)]
-        line[4] = int(line[4])
-        counter_arr.append(counter)
-        positivity_ratings.append(line[1])
-        negativity_ratings.append(line[2])
-        closing_prices.append(line[3])
-        counter+=1
+def plotTicker(names):
+    for name in names:
+        fig, ax = plt.subplots(nrows=2, constrained_layout=True)
+        fig.suptitle(name)
+        f = open("reddit_data.txt", "r")
         line = f.readline()
-    d1 = {'x': counter_arr, 'y': positivity_ratings}
-    d2 = {'x1': counter_arr, 'y1': closing_prices}
-    plt.plot('x', 'y', data=d1, marker='o', markerfacecolor='blue', markersize=12, color='skyblue', linewidth=4)
-    plt.plot('x1', 'y1', data=d2, marker='o', markerfacecolor='blue', markersize=12, color='olive', linewidth=2)
-    plt.show()
+        closing_prices = []
+        positivity_ratings = []
+        negativity_ratings = []
+        counter_arr = []
+        counter = 0
+        while(line):
+            if(line[0]!='['):
+                line = f.readline()
+                continue
+            line = line[1:-2].split(", ")
+            line[0] = line[0][1:-1]
+            if(line[0] != name):
+                line = f.readline()
+                continue
+            line[1:4] = [float(line[i]) for i in range(1,4)]
+            line[4] = int(line[4])
+            counter_arr.append(counter)
+            positivity_ratings.append(line[1])
+            negativity_ratings.append(line[2])
+            closing_prices.append(line[3])
+            counter+=1
+            line = f.readline()
+        d1 = {'counter': counter_arr, 'closing': closing_prices}
+        d2 = {'counter': counter_arr, 'positivity': positivity_ratings}
+        ax[0].title.set_text("Stock price")
+        ax[0].plot('counter', 'closing', data=d1, marker='o', markerfacecolor='blue', markersize=12, color='skyblue', linewidth=4)
+        ax[1].title.set_text("Positivity Rating")
+        ax[1].plot('counter', 'positivity', data=d2, marker='o', markerfacecolor='blue', markersize=12, color='olive', linewidth=2)
+        plt.show()
 
-plotTicker('AAPL')
+plotTicker(['TSLA', 'AAPL', 'NIO', 'AMD', 'BABA', 'AMZN', 'RIOT'])
